b08560a79d51a5b628f05c97fb786f153c120eaa
chen7549-cmd <pinhuanchen39@gmail.com>
2025-11-10 14:24:09 +0800

    Add files via upload

---
+++ f345. 新手練習題—陣列.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+    for (int i = 0; i < n; i++) cin >> vec[i];
+    for (auto it = vec.rbegin(); it != vec.rend(); ++it) {
+        cout << *it << " ";
+    }
+    return 0;
+}
